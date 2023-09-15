0d338d1eca23eb20af11fce689ce72ddf2396bb5
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2023-09-15 18:04:23 +0530

    Time: 2 ms (69.28%), Space: 47.7 MB (70.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        //The optimised approach is moore's voting algorithm
+        int ele = nums[0];
+        int count = 1;
+        for(int i = 1; i<nums.length; i++){
+            if(ele==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+            if(count==0){
+                ele = nums[i];
+                count = 1;
+            }
+        }
+        return ele;
     }
 }
