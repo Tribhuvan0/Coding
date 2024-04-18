d29be374e3c0d39b00d1313a8d2aeb01a7f6341d
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2024-04-18 17:31:35 +0530

    Time: 60 ms (61.39%), Space: 17.7 MB (56.20%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lst = []
+        #one pass solution
+        numMap = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if target == nums[i] + nums[j]:
-                    lst.append(i)
-                    lst.append(j)
-        return lst
+            number = target - nums[i]
+            if number in numMap:
+                return [i,numMap[number]]
+            numMap[nums[i]] = i
+        return []
                 
+    #Two pass solution
+    def twoSumPass(self, nums,target):
+        numMap = {}
+        for i in range(len(nums)):
+            numMap[nums[i]] = i
+        
+        for i in range(len(nums)):
+            number = target - nums[i]
+            if number in numMap and numMap[number] != i:
+                return [i, numMap[number]]
+        return []
         
