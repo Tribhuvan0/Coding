6f99352505dfc2d9b793f52a10b38a3f4b546a36
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2024-04-23 16:56:20 +0530

    Time: 40 ms (40.55%), Space: 16.6 MB (90.57%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,29 +1,10 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        dicti = {}
+        dicti = {'b':0,'a':0,'l':0,'o':0,'n':0}
         for i in text:
-            dicti[i] = dicti.get(i,0)+1
-
-        cnt = 0
-    
-        while dicti.get('b'):
-            stri = ''
-            if 'b' in dicti and dicti['b'] != 0:
-                dicti['b'] -= 1
-                stri += 'b'
-            if 'a' in dicti and dicti['a'] != 0:
-                dicti['a'] -= 1
-                stri += 'a'
-            if 'l' in dicti and dicti['l'] != 1 and dicti['l'] > 0:
-                dicti['l'] -= 2
-                stri += 'll'
-            if 'o' in dicti and dicti['o'] != 1 and dicti['o'] > 0:
-                dicti['o'] -= 2
-                stri += 'oo'
-            if 'n' in dicti and dicti['n'] != 0:
-                dicti['n'] -= 1
-                stri += 'n'
-            if stri == 'balloon':
-                cnt += 1
-        return cnt
+            if i in dicti:
+                dicti[i] += 1
+        dicti['l'] = dicti['l']//2
+        dicti['o'] = dicti['o']//2
+        return min(dicti.values())
         
