9752c588f903e0583aefaba21bdfbd46ec5c2b79
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2024-04-25 23:39:15 +0530

    Time: 38 ms (52.49%), Space: 16.6 MB (9.60%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        #Two pass solutions using bucket sort
+        cnt_lst = [0,0,0]
+        for i in nums:
+            if i == 0:
+                cnt_lst[0] += 1
+            elif i == 1:
+                cnt_lst[1] += 1
+            else:
+                cnt_lst[2] += 1
+        
+        for i in range(len(nums)):
+            if cnt_lst[0] != 0:
+                nums[i] = 0
+                cnt_lst[0] -= 1
+            elif cnt_lst[1] != 0:
+                nums[i] = 1
+                cnt_lst[1] -= 1
+            elif cnt_lst[2] != 0:
+                nums[i] = 2
+                cnt_lst[2] -= 1
+        
+            
         
