c8b0298ac740657f737c13b9acf7696217b8dd1c
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2024-04-26 00:05:44 +0530

    Time: 36 ms (64.32%), Space: 16.5 MB (42.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,5 +1,5 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def srtColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -23,6 +23,25 @@ class Solution:
             elif cnt_lst[2] != 0:
                 nums[i] = 2
                 cnt_lst[2] -= 1
-        
+    
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        #One pass solution using partition logic, we are not incrementing pointer
+        #as incrementing pointer in case of right_ptr can cause 0 to be undetected
+        left_ptr, right_ptr = 0, len(nums) - 1
+        i = 0
+        while i <= right_ptr:
+            if nums[i] == 0:
+                nums[left_ptr], nums[i] = nums[i], nums[left_ptr]
+                left_ptr += 1
+            
+            elif nums[i] == 2:
+                nums[right_ptr], nums[i] = nums[i], nums[right_ptr]
+                right_ptr -= 1
+                i -= 1
+            i += 1
+                
             
         
