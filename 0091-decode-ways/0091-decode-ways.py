c84c9cb8d40ec25799b792c89bbc2b9f2bc915f3
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2024-07-13 14:45:21 +0530

    Time: 39 ms (37.20%), Space: 16.5 MB (45.59%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -9,7 +9,7 @@ class Solution:
     
         for i in range(2,len(s)+1):
             #one digit
-            if 0 < int(s[i-1:i]) <= 9:
+            if 0 < int(s[i-1]) <= 9:
                 dp[i] += dp[i-1]
             if 10 <= int(s[i-2:i]) <= 26:
                 dp[i] += dp[i-2]
