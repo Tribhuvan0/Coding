b544d191cee9546758752d7efae6b311d32ae898
Tribhuvan Mishra <74012568+Tribhuvan0@users.noreply.github.com>
2024-07-13 15:15:49 +0530

    Time: 55 ms (57.27%), Space: 16.8 MB (20.27%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -4,13 +4,11 @@ class Solution:
             return 0
         
         dp = [0]*len(cost)
-        
         dp[0] = cost[0]
-        
-        if len(cost) >= 2:
-            dp[1] = cost[1]
+        dp[1] = cost[1] if len(cost) >= 2 else cost[0]
         
         for i in range(2,len(cost)):
             dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-            
+        
         return min(dp[-1], dp[-2])
+        
